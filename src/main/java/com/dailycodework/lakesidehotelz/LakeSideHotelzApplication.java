0087427f06d7679cab621210e1b12d52c8773c11
sandali <it21834424@my.sliit.lk>
2024-06-27 01:38:34 +0530

    #very first commit is added

---
+++ src/main/java/com/dailycodework/lakesidehotelz/LakeSideHotelzApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodework.lakesidehotelz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakeSideHotelzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LakeSideHotelzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dailycodework/lakesidehotelz/LakeSideHotelzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodework.lakesidehotelz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakeSideHotelzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
